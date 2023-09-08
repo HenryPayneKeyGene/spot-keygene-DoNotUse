a88ba37257933662b12035e7797343e83a362a30
romir kulshrestha <romir.kulshrestha@gmail.com>
2023-09-08 22:19:12 +0200

    copy

--- src/spot-keygene/blk.py
+++ src/spot-keygene/blk.py
@@ -1,3 +1,8 @@
+#  Copyright (c) Romir Kulshrestha 2023.
+#  You may use, distribute and modify this code under the terms of the MIT License.
+#  You should have received a copy of the MIT License with this file. If not, please visit:
+#  https://opensource.org/licenses/MIT
+
 from blk_arc_api.blk_arc_config import ConnectionType
 from blk_arc_api.blk_arc import BLK_ARC
 
--- src/spot-keygene/main.py
+++ src/spot-keygene/main.py
@@ -1,4 +1,7 @@
-from time import sleep
+#  Copyright (c) Romir Kulshrestha 2023.
+#  You may use, distribute and modify this code under the terms of the MIT License.
+#  You should have received a copy of the MIT License with this file. If not, please visit:
+#  https://opensource.org/licenses/MIT
 
 from .blk import BLK
 from .spot import Spot
